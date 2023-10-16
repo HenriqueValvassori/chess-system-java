c11fdd53a0d0c20fbcce0680b7217260ffaf1309
Henrique Valvassori <henriquevalvassori41@gmail.com>
2023-10-16 12:45:16 -0300

    First class position

---
+++ src/boardgame/position.java
@@ -0,0 +1,31 @@
+package boardgame;
+
+public class position {
+	
+	private int rows;
+	private int column;
+	public position(int rows, int column) {
+		
+		this.rows = rows;
+		this.column = column;
+	}
+	public int getRows() {
+		return rows;
+	}
+	public void setRows(int rows) {
+		this.rows = rows;
+	}
+	public int getColumn() {
+		return column;
+	}
+	public void setColumn(int column) {
+		this.column = column;
+	}
+	
+	@Override
+	public String toString() {
+		return rows + " , " + column;
+	}
+	
+	
+}
